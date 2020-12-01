48175cfadd2c28d8c63fe44aeb85f328452eaf7f
Вячеслав Андреев <maxivimax@icloud.com>
2020-12-01 18:40:48 +0300

    Updates

---
+++ precache-manifest.13c25e58f9a26da84bf4c0a4ea386a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4df40b0473bf5b7c73a731225495703",
+    "url": "/chatt/index.html"
+  },
+  {
+    "revision": "e8e2a38540a59ff484cb",
+    "url": "/chatt/static/css/main.5a567a92.chunk.css"
+  },
+  {
+    "revision": "6c0b914d639a05088de7",
+    "url": "/chatt/static/js/2.f1b23ada.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/chatt/static/js/2.f1b23ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e2a38540a59ff484cb",
+    "url": "/chatt/static/js/main.8f9f1260.chunk.js"
+  },
+  {
+    "revision": "8b04133457a93bca2834",
+    "url": "/chatt/static/js/runtime-main.52aeaee9.js"
+  }
+]);
